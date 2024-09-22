656504e445b942b41a2f41f3b7ce89bfa75fe2a5
Asadullah Khan <67080107+Khanmdasad@users.noreply.github.com>
2024-09-22 21:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //your JS code here. If required.
 const student = {
-	name: Asadullah,
-	
+	name: Asadullah,	
 }
 
 Object.prototype.getKeys = function() {
@@ -9,3 +8,26 @@ Object.prototype.getKeys = function() {
 }
 
 console.log(student.getKeys());
+
+
+// Ensure Object.prototype.getKeys is available in the environment before test runs
+beforeEach(() => {
+  Object.prototype.getKeys = function() {
+    return Object.keys(this);
+  };
+});
+
+it('should fetch and test the script', () => {
+  cy.visit(baseUrl + "/main.html"); // Ensure visit is set correctly
+  cy.document().then(doc => {
+    const script = doc.querySelector('script[src="./script.js"]');
+    const scriptUrl = script.getAttribute("src");
+    cy.readFile(scriptUrl).then(jsCode => {
+      eval(jsCode); // Evaluate the script content
+      
+      const myObj = { name: "John", age: 30, city: "New York" };
+      const keys = myObj.getKeys(); // Use getKeys()
+      expect(keys).to.deep.equal(["name", "age", "city"]);
+    });
+  });
+});
